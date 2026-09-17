648341fa2d2d227e61050041934977635d484967
absiddiq <siddiq.aiub@gmail.com>
2026-09-17 22:15:19 +0600

    create square component for square and handle clicking

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
 function Board() {
   return (
-    <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-        X
-      </button>
-    </div>
+    <>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
 
